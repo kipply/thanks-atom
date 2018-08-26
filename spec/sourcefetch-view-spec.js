c94ac7746255aeb201795bb4a5797b2178de77f9
kipply <carolyfisher@gmail.com>
2018-08-26 05:43:46 -0400

    waw

---
+++ spec/sourcefetch-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import polycodeView from '../lib/polycode-view';
+
+describe('polycodeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
